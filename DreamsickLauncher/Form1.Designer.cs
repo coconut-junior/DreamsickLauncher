3acd11f0e28438518cb6f3df44c7473c50213802
Jimmy Blanck <blanckjm@gmail.com>
2020-04-22 20:42:53 -0400

    Add project files.

---
+++ DreamsickLauncher/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DreamsickLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 127);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 82);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(683, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 44);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Play";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.UseCompatibleTextRendering = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(633, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Settings";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.UseCompatibleTextRendering = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(689, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 44);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Exit";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.UseCompatibleTextRendering = true;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(636, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 44);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Website";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dreamsick Launcher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DreamsickLauncher/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace DreamsickLauncher
+{
+    public partial class Form1 : Form
+    {
+        settings s = new settings();
+        private PrivateFontCollection pfc = new PrivateFontCollection();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            label1.MouseMove += Label_MouseMove;
+            label1.MouseLeave += Label_MouseLeave;
+            label2.MouseMove += Label_MouseMove;
+            label2.MouseLeave += Label_MouseLeave;
+            label3.MouseMove += Label_MouseMove;
+            label3.MouseLeave += Label_MouseLeave;
+            label4.MouseMove += Label_MouseMove;
+            label4.MouseLeave += Label_MouseLeave;
+
+            Stream fontStream = this.GetType().Assembly.GetManifestResourceStream("DreamsickLauncher.kenyan coffee rg.ttf");
+            byte[] fontdata = new byte[fontStream.Length];
+            fontStream.Read(fontdata, 0, (int)fontStream.Length);
+            fontStream.Close();
+
+            unsafe
+            {
+                fixed (byte* pFontData = fontdata)
+                {
+                    pfc.AddMemoryFont((System.IntPtr)pFontData, fontdata.Length);
+                }
+            }
+
+            foreach (Control c in this.Controls)
+            {
+                if (c is Label l)
+                {
+                    l.Font = new Font(pfc.Families[0], 30, FontStyle.Regular);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            s.Show();
+        }
+
+        private void Label_MouseLeave(object sender, EventArgs e)
+        {
+            if (sender is Label label)
+            {
+                label.ForeColor = Color.Gray;
+            }
+        }
+
+        private void Label_MouseMove(object sender, EventArgs e)
+        {
+            if (sender is Label label)
+            {
+                label.ForeColor = Color.FromArgb(137, 197, 65);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
